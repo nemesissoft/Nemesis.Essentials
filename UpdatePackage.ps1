2d4cb0f354239005ef87e8ab34e323b356498560
Michał Bryłka <michal.brylka@gmail.com>
2019-09-26 22:16:28 +0200

    Try to obtain commit message for appveyor

---
+++ UpdatePackage.ps1
@@ -0,0 +1,34 @@
+﻿param (
+  [string]$basePath = "",
+  [string]$releaseNotes = ""
+)
+
+function Update-Package([string]$packagesFileName, [string] $newReleaseNotes) 
+{
+    $nuspecTemplate = [xml](cat $packagesFileName)
+    $metadata = $nuspecTemplate.package.metadata
+    
+    if($metadata.releaseNotes) {
+        $metadata.releaseNotes = "$newReleaseNotes"
+    }
+    else {
+        $newNode = $metadata.OwnerDocument.CreateElement("releaseNotes")
+        $newNode.InnerText = "$newReleaseNotes"
+        $metadata = $metadata.AppendChild($newNode)
+    }        
+
+    $nuspecTemplate.Save((get-item $packagesFileName))
+}
+
+Write-Output ("Attempt to change release notes to: " + $releaseNotes)
+$files = Get-ChildItem -Path "$basePath" -Filter *.nuspec -Recurse
+
+if ($files.Count -ne 0)
+{
+    foreach($file in $files)
+    {
+        Write-Output ("PATCHING " + $file.FullName)
+        
+        Update-Package -packagesFileName $file.FullName -newReleaseNotes "$releaseNotes"
+    }            
+}
