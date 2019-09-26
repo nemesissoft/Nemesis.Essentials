ddc50bbf75c2c9fd3091e6e63caa968d78698ed5
Michał Bryłka <michal.brylka@gmail.com>
2019-09-26 22:16:28 +0200

    Try to obtain commit message for appveyor

---
+++ UpdateReleaseNotes.ps1
@@ -0,0 +1,34 @@
+﻿param (
+  [string]$basePath = "",
+  [string]$releaseNotes = ""
+)
+
+#<PackageReleaseNotes>RELEASE_NOTES_PLACEHOLDER<!--starts with "RELEASE_NOTES_PLACEHOLDER" == this will be patched by AppVeyor--></PackageReleaseNotes>
+function Update-ReleaseNotes([string]$csProjFileName, [string] $newReleaseNotes) 
+{
+    $csProjDoc = [xml](Get-Content $csProjFileName)
+    $releaseNotesNode = $csProjDoc.SelectSingleNode("Project/PropertyGroup/PackageReleaseNotes")
+        
+    if($releaseNotesNode.InnerText -clike "RELEASE_NOTES_PLACEHOLDER*") 
+    {
+        Write-Output ("PATCHING '" + $csProjFileName + "' with '" + $newReleaseNotes + "'")
+        $releaseNotesNode.InnerText = "$newReleaseNotes"
+        $csProjDoc.Save($csProjFileName)
+    }    
+}
+
+try {
+    $files = Get-ChildItem -Path "$basePath" -Filter *.csproj -Recurse
+    if ($files.Count -ne 0)
+    {
+        foreach($file in $files)
+        {   
+            Update-ReleaseNotes -csProjFileName $file.FullName -newReleaseNotes "$releaseNotes"
+        }            
+    }
+}
+catch {
+    Write-Error "An error occurred during patching files"
+    Write-Error $_
+    Exit
+}
