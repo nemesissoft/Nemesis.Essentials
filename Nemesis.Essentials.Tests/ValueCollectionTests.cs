3b85061ff55b89ce09db4a18a9d8c7784d2ed883
Michał Bryłka <michal.brylka@gmail.com>
2020-11-17 22:33:19 +0100

    Add value semmantics collection

--- Nemesis.Essentials.Tests/ValueCollectionTests.cs
+++ Nemesis.Essentials.Tests/ValueCollectionTests.cs
@@ -6,11 +6,11 @@ using Nemesis.Essentials.Design;
 
 namespace Nemesis.Essentials.Tests
 {
-    [TestFixture(TestOf = typeof(EnumerableEqualityComparer<>))]
-    public class EnumerableEqualityComparerTests
+    [TestFixture(TestOf = typeof(ValueCollection<>))]
+    public class ValueCollectionTests
     {
-        private static IList<string> Empty() => new List<string>();
-        private static IList<string> From(params string[] elements) => new List<string>(elements);
+        private static ValueCollection<string> Empty() => new ValueCollection<string>();
+        private static ValueCollection<string> From(params string[] elements) => new ValueCollection<string>(elements.ToList());
         [SuppressMessage("ReSharper", "PossibleMultipleEnumeration")]
         private static string Format(IEnumerable<string> list) =>
             list == null ? "NULL" :
@@ -26,73 +26,58 @@ namespace Nemesis.Essentials.Tests
         {
             (null, null),
             (Empty(), Empty()),
-            (Empty().AddFluent("1"),  Empty().AddFluent("1")),
-            (Empty().AddFluent("1", "2"),  Empty().AddFluent("1").AddFluent("2")),
-            (Empty().AddFluent("1", "2").AddFluent("3"),  Empty().AddFluent("1").AddFluent("2").AddFluent(3.ToString())),
+            (Empty().AddFluently("1"),  Empty().AddFluently("1")),
+            (Empty().AddFluently("1", "2"),  Empty().AddFluently("1").AddFluently("2")),
+            (Empty().AddFluently("1", "2").AddFluently("3"),  Empty().AddFluently("1").AddFluently("2").AddFluently(3.ToString())),
             (From("1", "2", "3"), From("1", "2", "3")),
-            (From("1", "2", "3", "4"), From("1", "2", "3").AddFluent("4")),
+            (From("1", "2", "3", "4"), From("1", "2", "3").AddFluently("4")),
         }.DuplicateWithElementReversal()
             .Select((elem, i) => new TestCaseData(elem).SetName($"{i + 1:00}. {Format(elem.Item1)} == {Format(elem.Item2)}"));
 
         [TestCaseSource(nameof(GetPositiveCases))]
-        public void Equals_Positive((IList<string> left, IList<string> right) data)
+        public void Equals_Positive((ValueCollection<string> left, ValueCollection<string> right) data)
         {
             var (left, right) = data;
             if (ReferenceEquals(left, right) && left != null && right != null)
                 Assert.Fail("Reference equality should be checked using trivial path");
 
-            var comparer = EnumerableEqualityComparer<string>.DefaultInstance;
-
-            Assert.That(left, Is.EqualTo(right).Using(comparer), "Equals assert");
 
             Assert.That(
-                comparer.GetHashCode(left), Is.EqualTo(
-                comparer.GetHashCode(right)
+                left?.GetHashCode() ?? int.MinValue, Is.EqualTo(
+                    right?.GetHashCode() ?? int.MinValue
                 ), "GetHashCode");
 
-            Assert.That(comparer.Equals(left, right), Is.True, "Equals");
+            Assert.That(left, Is.EqualTo(right), "Equals assert");
         }
 
 
         private static IEnumerable<TestCaseData> GetNegativeCases() => new[]
             {
                 (Empty(), null),
-                (Empty().AddFluent("1"), null),
+                (Empty().AddFluently("1"), null),
                 (From("1", "2", "3", "4"), From("1", "2", "3")),
                 (From("1", "2", "3", ""), From("1", "2", "3")),
-                (From("1", "2", "3", "4", "5"), From("1", "2", "3").AddFluent("4")),
-                (From("1", "2", "3", "4", "5", "6"), From("1", "2", "3").AddFluent("4")),
+                (From("1", "2", "3", "4", "5"), From("1", "2", "3").AddFluently("4")),
+                (From("1", "2", "3", "4", "5", "6"), From("1", "2", "3").AddFluently("4")),
             }.DuplicateWithElementReversal()
             .Select((elem, i) => new TestCaseData(elem).SetName($"{i + 1:00}. {Format(elem.Item1)} != {Format(elem.Item2)}"));
 
         [TestCaseSource(nameof(GetNegativeCases))]
-        public void Equals_Negative((IList<string> left, IList<string> right) data)
+        public void Equals_Negative((ValueCollection<string> left, ValueCollection<string> right) data)
         {
             var (left, right) = data;
-            var comparer = EnumerableEqualityComparer<string>.DefaultInstance;
-
-            Assert.That(left, Is.Not.EqualTo(right).Using(comparer), "Equals assert");
-
-            Assert.That(comparer.Equals(left, right), Is.False, "Equals");
+            
+            Assert.That(left, Is.Not.EqualTo(right), "Equals assert");
         }
     }
 
-    internal static class EnumerableEqualityComparerTestsHelper
+    internal static class ValueCollectionHelper
     {
-        internal static IList<T> AddFluent<T>(this IList<T> list, params T[] elements)
+        internal static ValueCollection<T> AddFluently<T>(this ValueCollection<T> list, params T[] elements)
         {
             foreach (var element in elements)
                 list.Add(element);
             return list;
-        }
-
-        internal static IEnumerable<(T, T)> DuplicateWithElementReversal<T>(this IList<(T, T)> list)
-        {
-            foreach (var t in list)
-                yield return t;
-
-            for (int i = 0; i < list.Count; i++)
-                yield return (list[i].Item2, list[i].Item1);
         }
     }
 }
