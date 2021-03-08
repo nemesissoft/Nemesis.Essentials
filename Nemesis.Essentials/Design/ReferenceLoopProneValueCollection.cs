4b8146b9f365547ee090af5e7cd4f1b007df71bb
Jan Pluskal <jan.pluskal@gmail.com>
2021-03-08 21:17:05 +0100

    Add ReferenceLoopProneValuesCollection.
    ToString override checks for call loop and breaks them.

---
+++ Nemesis.Essentials/Design/ReferenceLoopProneValueCollection.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Nemesis.Essentials.Design
+{
+    public class ReferenceLoopProneValueCollection<T> : ValueCollection<T>
+    {
+        private const string LoopDetectedNotification = "## SELF REFERENCING LOOP DETECTED ##";
+
+        public ReferenceLoopProneValueCollection() : base(new List<T>()) { }
+
+        public ReferenceLoopProneValueCollection(IEqualityComparer<T> equalityComparer = null) : base(new List<T>(), equalityComparer) { }
+
+        public ReferenceLoopProneValueCollection(IList<T> list, IEqualityComparer<T> equalityComparer = null) : base(list) { }
+
+        public override string ToString() => CheckSelfReferencingLoop() ?? base.ToString();
+
+        private static string CheckSelfReferencingLoop()
+        {
+            StackTrace stackTrace = new();
+            var callerMethodHandleValue = stackTrace.GetFrame(1)?.GetMethod()?.MethodHandle.Value ?? default;
+            return stackTrace.GetFrames().Skip(2).Any(f => f.GetMethod()?.MethodHandle.Value == callerMethodHandleValue)
+                ? LoopDetectedNotification
+                : null;
+        }
+    }
+}
