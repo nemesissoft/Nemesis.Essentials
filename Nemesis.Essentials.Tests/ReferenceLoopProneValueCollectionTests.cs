b8cc6217a61ecea93e94e6769a9ab86c804b4f87
Jan Pluskal <jan.pluskal@gmail.com>
2021-03-08 22:04:33 +0100

    Add ReferenceLoopProneValueCollectionTests.

---
+++ Nemesis.Essentials.Tests/ReferenceLoopProneValueCollectionTests.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using Nemesis.Essentials.Design;
+using NUnit.Framework;
+
+namespace Nemesis.Essentials.Tests
+{
+    [TestFixture(TestOf = typeof(ReferenceLoopProneValueCollection<>))]
+    public class ReferenceLoopProneValueCollectionTests
+    {
+        private const string LoopDetectedNotification = "## SELF REFERENCING LOOP DETECTED ##";
+
+        public record A
+        {
+            public ICollection<B> Collection = new ReferenceLoopProneValueCollection<B>();
+        }
+
+        public record B (A A)
+        {
+            public A A { get; } = A;
+        }
+
+        [Test]
+        public void LoopDetected()
+        {
+            //Arrange
+            var a = new A();
+            var b = new B(a);
+            a.Collection.Add(b);
+
+            //Act
+            var aToString = a.ToString();
+
+            //Assert
+            Assert.True(aToString.Contains(LoopDetectedNotification));
+        }
+
+        [Test]
+        public void LoopNotDetected()
+        {
+            //Arrange
+            var a = new A();
+            var b = new B(null);
+            a.Collection.Add(b);
+
+            //Act
+            var aToString = a.ToString();
+
+            //Assert
+            Assert.False(aToString.Contains(LoopDetectedNotification));
+        }
+    }
+}
