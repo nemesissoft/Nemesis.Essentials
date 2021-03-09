bef2bef27a07ed6d11123c415fd4cc094962bf5c
Jan Pluskal <jan.pluskal@gmail.com>
2021-03-09 21:01:32 +0100

    Add ReferenceLoopProneValueCollection (#4)
    * Add ReferenceLoopProneValuesCollection.
    
    ToString override checks for call loop and breaks them.
    
    * Add ReferenceLoopProneValueCollectionTests.
    
    * Reimplement ReferenceLoopProneValueCollection as decorator.
    
    * Make LoopDetectedNotification internal and reuse in tests.

---
+++ Nemesis.Essentials.Tests/ReferenceLoopProneValueCollectionTests.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using Nemesis.Essentials.Design;
+using NUnit.Framework;
+
+namespace Nemesis.Essentials.Tests
+{
+    [TestFixture(TestOf = typeof(ReferenceLoopProneValueCollection<>))]
+    public class ReferenceLoopProneValueCollectionTests
+    {
+        public record A
+        {
+            public ICollection<B> Collection = new ReferenceLoopProneValueCollection<B>();
+        }
+
+        public record B (A A)
+        {
+            public A A { get; } = A;
+        }
+
+        [Test]
+        public void LoopDetected()
+        {
+            //Arrange
+            var a = new A();
+            var b = new B(a);
+            a.Collection.Add(b);
+
+            //Act
+            var aToString = a.ToString();
+
+            //Assert
+            Assert.True(aToString.Contains(ReferenceLoopProneValueCollection<B>.LoopDetectedNotification));
+        }
+
+        [Test]
+        public void LoopNotDetected()
+        {
+            //Arrange
+            var a = new A();
+            var b = new B(null);
+            a.Collection.Add(b);
+
+            //Act
+            var aToString = a.ToString();
+
+            //Assert
+            Assert.False(aToString.Contains(ReferenceLoopProneValueCollection<B>.LoopDetectedNotification));
+        }
+    }
+}
